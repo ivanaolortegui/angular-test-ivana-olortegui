76ab91858cffd44e74a8f7f39ce58d4841da7e09
ivanaolorteguimoreno <iolortegui@proveedor.intercorp.com.pe>
2022-06-12 10:05:10 -0400

    Form validation ✔

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
@@ -8,10 +8,35 @@ import { ErrorStateMatcher } from '@angular/material/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent implements OnInit {
+  formSignup: FormGroup;
 
-  constructor() { }
+  @Input('value') inputValue: any;
+  @Output('value') outputValue: EventEmitter<any> = new EventEmitter();
+  constructor(private fb: FormBuilder) {
+
+    this.formSignup = this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      lastname: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{5,}')
+      ]]
+    });
+  }
+
 
   ngOnInit(): void {
+
+  }
+
+  ngOnChanges(changes: any) {
+    if (!changes.inputValue.firstChange) {
+
+      this.formSignup.setValue({ ...this.inputValue })
+    }
+    console.log(changes);
   }
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
@@ -19,13 +44,16 @@ export class ChildComponent implements OnInit {
   matcher = new MyErrorStateMatcher();
 
   register() {
-    const data = {
-      name: 'Ivana', lastname: 'Olortegui', email: 'angular@qservus.com',
-      password: 'angularV13'
-    }
-    console.log(data);
 
+    const data = this.formSignup.value;
+    /*  const data = {
+       name: 'Jessica', lastname: 'Olortegui', email: 'angular@qservus.com',
+       password: 'angularV13'
+     } */
+    this.outputValue.emit(data);
+    console.log(data);
   }
+
 
 }
 
--- src/app/pages/parent/parent.component.ts
+++ src/app/pages/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  valueChange: any;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addRegister(value: any) {
+    console.log(value);
+  }
+
+  valueChanges() {
+    this.valueChange = {
+      name: 'Ivana', lastname: 'Olortegui', email: 'angular@qservus.com',
+      password: 'angularV13'
+    }
+  }
+
 
 }
